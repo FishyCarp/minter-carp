5edf7941e42b2e5b95d62a26e0518d5a83296b37
FishyCarp <radio.prectorian@gmail.com>
2022-04-09 20:52:36 +0700

    new

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0x879F95DFe750bB0478eecBCC2A9835DEdFA255f0"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x08CC8b318f62BA9edA19D47f406Dfc95558B3214"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Home of the Tanuki Kuji Collection";
 const welcome_h2 = "Connect to MetaMask or CoinBase to Get Started";

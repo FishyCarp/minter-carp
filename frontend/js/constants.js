b57a1d69729835111683cfbb961f41216e8dd235
FishyCarp <radio.prectorian@gmail.com>
2022-04-09 17:57:04 +0700

    append

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x879F95DFe750bB0478eecBCC2A9835DEdFA255f0"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
